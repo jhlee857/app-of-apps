89cec25689adb396b730e66eef88edd905f71b2e
ljh <ljho0507@gmail.com>
2023-12-13 20:19:25 +0900

    test

--- curl.sh
+++ curl.sh
@@ -1,11 +1,20 @@
 #!/bin/bash
-while :
+#while :
+SET=$(seq 0 999)
+success_cnt=0
+failed_cnt=0
+for i in $SET
 do
 STATUS=$(curl -s -o /dev/null -w "%{http_code}" "nginx.localhost:9080")
 
 if [ $STATUS -eq 200 ]; then
-  echo "OKOK"
+  echo $STATUS
+  success_cnt=$((success_cnt+1))
 else
-  echo "Failed"
+  echo "!!!!! Failed !!!!!!" - $STATUS
+  failed_cnt=$((failed_cnt+1))
 fi
 done
+
+echo success : $success_cnt
+echo failed : $failed_cnt
